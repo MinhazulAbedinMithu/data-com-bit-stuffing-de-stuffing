b1a1d90e6638059098fd85fac0c1c40c2fa9feae
Minhazul Abedin Mithu <minhazulabedinmithu@gmail.com>
2021-02-25 23:49:48 +0600

    byte added

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 document.getElementById('convert').addEventListener('click', function(event) {
-    let inputBits = document.getElementById('input').value;
-    inputBits = inputBits.split('');
+    let inputStr = document.getElementById('input').value;
+    inputStr = inputStr.split('');
     const transOption = document.getElementById('transOption').value;
     
-    if(transOption == 'Stuffing') {
-        bitStuffing(inputBits);
-    }
-    else if(transOption == 'De Stuffing') {
-        bitDeStuffing(inputBits);
+    if(transOption == 'Bit Stuffing') {
+        bitStuffing(inputStr);
+    }else if(transOption == 'Bit De Stuffing') {
+        bitDeStuffing(inputStr);
+    }else if(transOption == 'Byte Stuffing') {
+        byteStuffing(inputStr);
+    }else if(transOption == 'Byte De Stuffing') {
+        byteDeStuffing(inputStr);
     }
 });
 
-const bitStuffing = inputBits => {
-    let bitsLength = inputBits.length;
+const bitStuffing = inputStr => {
+    let bitsLength = inputStr.length;
     let count = 0;
     for (let i = 0; i < bitsLength; i++) {
-        if(inputBits[i] === '1'){
+        if(inputStr[i] === '1'){
             count += 1;
         }else{
             count = 0;
@@ -23,22 +26,22 @@ const bitStuffing = inputBits => {
 
         if(count == 5) {
             for (let j = bitsLength; j > i; j--) {
-                inputBits[j] = inputBits[j-1];
+                inputStr[j] = inputStr[j-1];
             }
-            inputBits[i+1] = '0';
+            inputStr[i+1] = '0';
             bitsLength += 1;
             count = 0;
         }
     }
-    inputBits = inputBits.join('');
-    document.getElementById('output').value = inputBits;
+    inputStr = inputStr.join('');
+    document.getElementById('output').value = inputStr;
 };
 
-const bitDeStuffing = inputBits => {
-    let bitsLength = inputBits.length;
+const bitDeStuffing = inputStr => {
+    let bitsLength = inputStr.length;
     let count = 0;
     for (let i = 0; i < bitsLength; i++) {
-        if(inputBits[i] === '1'){
+        if(inputStr[i] === '1'){
             count += 1;
         }else{
             count = 0;
@@ -46,12 +49,52 @@ const bitDeStuffing = inputBits => {
 
         if(count == 5) {
             for (let j = i+1; j < bitsLength; j++) {
-                inputBits[j] = inputBits[j+1];
+                inputStr[j] = inputStr[j+1];
             }
             bitsLength -= 1;
             count = 0;
         }
     }
-    inputBits = inputBits.join('');
-    document.getElementById('output').value = inputBits;
+    inputStr = inputStr.join('');
+    document.getElementById('output').value = inputStr;
 };
+
+
+
+
+
+// let string = 'minhazul abedin mithu';
+const flag = 'z';
+const esc = 'm';
+
+function byteStuffing(inputStr) {
+    const stringLength = inputStr.length;
+    let stuffed = [];
+
+    let count = 0;
+    for (let i = 0; i < stringLength; i++, count++) {
+        if(inputStr[i] === flag || inputStr[i] === esc) {
+            stuffed[count] = esc;
+            count++;
+            stuffed[count] = inputStr[i];
+        }else{
+            stuffed[count] = inputStr[i];
+        }
+    }
+    inputStr = stuffed.join('');
+    document.getElementById('output').value = inputStr;
+}
+//const stuffedString = byteStuffing(string);
+
+function byteDeStuffing(inputStr) {
+    const stringLength = inputStr.length;
+    for (let i = 0; i < stringLength; i++) {
+        if(inputStr[i] == esc && (inputStr[i+1] == esc || inputStr[i+1] == flag)){
+            inputStr.splice(i, 1);
+        }
+    }
+    inputStr =  inputStr.join('');
+    document.getElementById('output').value = inputStr;
+}
+// const deStuffedString = byteDeStuffing(stuffedString);
+
